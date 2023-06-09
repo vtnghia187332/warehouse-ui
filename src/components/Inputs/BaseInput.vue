0ddc78ac6388130815fd7ec367dfbe2393cd25b0
vtnghia1 <vtnghia110@gmail.com>
2023-06-09 23:51:00 +0700

    create base input

---
+++ src/components/Inputs/BaseInput.vue
@@ -0,0 +1,50 @@
+
+<template>
+    <div class="mb-2">
+        <label v-if="field.label" :for="field.id" class="!font-bold block">
+            {{ field.label }}
+        </label>
+        <input :id="field.id" :type="field.type" :value="value" @input="updateInput"
+            class="relative border rounded-sm w-full p-1 focus:outline-none focus:ring-2 focus:[007bff] focus:border-transparent"
+            :class="field.error ? `${field.classes} !border-red-500` : field.classes" />
+        <div class="absolute mb-1 text-red-500">{{ field.error }}</div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "BaseInput",
+    props: {
+        id: {
+            type: String,
+            default: "",
+        },
+        label: {
+            type: String,
+            default: "",
+        },
+        value: {
+            type: [String, Number],
+            default: "",
+        },
+        type: {
+            type: String,
+            default: "text",
+        },
+        classes: String,
+        error: {
+            type: String,
+        },
+        field: {
+            type: Object,
+            default: {},
+        },
+    },
+    methods: {
+        updateInput(event) {
+            this.$emit("input", event.target.value);
+        },
+    },
+};
+</script>
+  
--- src/pages/warehouse/WarehouseDetail.vue
+++ src/pages/warehouse/WarehouseDetail.vue
@@ -7,25 +7,22 @@
             <div class="flex justify-between">
               <div>
                 <h4 class="mb-2">Warehouse Code</h4>
-                <el-input
-                  class="w-[550px]"
-                  placeholder="Please input"
-                  v-model="input"
-                >
+                <el-input class="w-[550px]" placeholder="Please input" v-model="input">
                 </el-input>
               </div>
               <div>
                 <h4 class="mb-2">Warehouse Code</h4>
-                <el-input
-                  class="w-[550px]"
-                  placeholder="Please input"
-                  v-model="input"
-                >
+                <el-input class="w-[550px]" placeholder="Please input" v-model="input">
                 </el-input>
               </div>
             </div>
           </template>
         </FormCard>
+        <BaseInput :field="warehouse.name" v-model="warehouse.name.value" />
+        <BaseInput :field="warehouse.email" v-model="warehouse.email.value" />
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="checkData">
+          Button
+        </button>
       </div>
       <div class="flex flex-col gap-y-4 min-w-[380px] w-[380px] h-full">
         <div class="p-3 bg-white radius-shadow_add">
@@ -40,9 +37,45 @@
 </template>
 
 <script>
+import BaseInput from "./../../components/Inputs/BaseInput.vue";
 import FormCard from "./../../components/Cards/FormCard.vue";
+import Button from "../../components/Button.vue";
 export default {
-  components: { FormCard },
+  components: { FormCard, BaseInput, Button },
+  data() {
+    return {
+      warehouse: {
+        name: {
+          id: "warehouseName",
+          classes: "!w-[100px]",
+          type: "text",
+          label: "Warehouse Name",
+          value: "",
+          error: "",
+        },
+        email: {
+          id: "warehouseEmail",
+          classes: "!w-[100px]",
+          type: "text",
+          label: "Warehouse Email",
+          value: "",
+          error: "",
+        },
+      },
+    };
+  },
+  methods: {
+    checkData() {
+      Object.keys(this.warehouse).forEach((key) => {
+        console.log(key);
+        if (this.warehouse[key].value == "") {
+          this.warehouse[key].error = this.warehouse[key].label.concat(" is not empty");
+        } else {
+          this.warehouse[key].error = "";
+        }
+      });
+    },
+  },
 };
 </script>
 
