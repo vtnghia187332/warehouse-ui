199003f48ace032fe1d1bf7e54d9be91447aeac1
vtnghia1 <vtnghia110@gmail.com>
2023-06-10 14:34:24 +0700

    key contact

--- src/pages/warehouse/WarehouseDetail.vue
+++ src/pages/warehouse/WarehouseDetail.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bg-page-background overflow-y-auto">
     <div class="p-4 flex gap-x-4">
-      <div class="forms grow flex flex-col gap-y-4">
+      <div class="w-[1130px] forms grow flex flex-col gap-y-4">
         <FormCard title="Information">
           <template v-slot:content>
-            <div>
+            <div class="!w-[1168px]">
               <div>
                 <BaseInput :field="warehouse.code" v-model="warehouse.code.value" />
               </div>
@@ -20,6 +20,42 @@
             </div>
           </template>
         </FormCard>
+        <div class="pt-3 bg-white radius-shadow_add">
+          <div class="px-3 text-primary-85 font-bold text-base">Key Contact</div>
+          <div class="mt-3 px-[15px] border-t border-b border-divider content-rate-card">
+            <div class="flex justify-between py-3">
+              <span class="text-sm font-bold flex items-center">Key Contact Person (3/3)
+              </span>
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Add
+              </button>
+            </div>
+            <div class="bg-neutral-2 w-full border border-neutral-5 rounded-sm mb-4">
+              <div class="flex justify-between border-b p-3">
+                <span class="text-sm font-bold flex items-center">Key Contact Person
+                </span>
+                <div class="cursor-pointer">
+                  <button class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Delete
+                  </button>
+                </div>
+              </div>
+              <div class="p-3">
+                <div class="flex justify-between">
+                  <BaseInput :field="keyContact.title" v-model="keyContact.title.value" />
+                </div>
+                <div class="flex justify-between">
+                  <BaseInput :field="keyContact.firstName" v-model="keyContact.firstName.value" />
+                  <BaseInput :field="keyContact.lastName" v-model="keyContact.lastName.value" />
+                </div>
+                <div class="flex justify-between">
+                  <BaseInput :field="keyContact.email" v-model="keyContact.email.value" />
+                  <BaseInput :field="keyContact.phoneNumber" v-model="keyContact.phoneNumber.value" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="checkData">
           Button
@@ -49,9 +85,10 @@ export default {
       warehouse: {
         code: {
           id: "warehouseCode",
-          classes: "!w-[560px]",
+          classes: "!w-[544px]",
           type: "text",
-          label: "Warehouse Code",
+          label: "Code",
+          isRequired: 'true',
           value: "",
           placeholder: "Enter Warehouse Code",
           maxlength: 20,
@@ -59,9 +96,10 @@ export default {
         },
         name: {
           id: "warehouseName",
-          classes: "!w-[1200px] !h-[64px]",
+          classes: "!w-[1168px] !h-[64px]",
           type: "text",
-          label: "Warehouse Name",
+          label: "Name",
+          isRequired: 'true',
           value: "",
           placeholder: "Enter Warehouse Name",
           maxlength: 150,
@@ -69,9 +107,9 @@ export default {
         },
         shortName: {
           id: "warehouseShortName",
-          classes: "!w-[1200px]",
+          classes: "!w-[1168px]",
           type: "text",
-          label: "Warehouse Short Name",
+          label: "Short Name",
           value: "",
           placeholder: "Enter Warehouse Short Name",
           maxlength: 100,
@@ -79,9 +117,10 @@ export default {
         },
         description: {
           id: "warehouseDescription",
-          classes: "!w-[1200px] !h-[64px]",
+          classes: "!w-[1168px] !h-[64px]",
           type: "text",
-          label: "Warehouse Description",
+          label: "Description",
+          isRequired: 'true',
           value: "",
           placeholder: "Enter Warehouse Description",
           maxlength: 150,
@@ -89,16 +128,79 @@ export default {
         },
 
       },
+      keyContact: {
+        title: {
+          id: "keycontactTitle",
+          classes: "!w-[540px]",
+          type: "text",
+          label: "Title",
+          isRequired: 'true',
+          value: "",
+          placeholder: "Enter Keycontact Title",
+          maxlength: '',
+          error: "",
+        },
+        firstName: {
+          id: "keycontactFirstName",
+          classes: "!w-[540px]",
+          type: "text",
+          label: "Frist Name",
+          isRequired: 'true',
+          value: "",
+          placeholder: "Enter Keycontact FristName",
+          maxlength: 30,
+          error: "",
+        },
+        lastName: {
+          id: "keycontactLastName",
+          classes: "!w-[540px]",
+          type: "text",
+          label: "Last Name",
+          isRequired: 'true',
+          value: "",
+          placeholder: "Enter Keycontact LastName",
+          maxlength: 30,
+          error: "",
+        },
+        email: {
+          id: "keycontactEmail",
+          classes: "!w-[540px]",
+          type: "text",
+          label: "Email",
+          isRequired: 'true',
+          value: "",
+          placeholder: "Enter Keycontact Email",
+          maxlength: 150,
+          error: "",
+        },
+        phoneNumber: {
+          id: "keycontactPhoneNo",
+          classes: "!w-[540px]",
+          type: "text",
+          label: "Phone Number",
+          isRequired: 'true',
+          value: "",
+          placeholder: "Enter Keycontact Phone Number",
+          maxlength: 150,
+          error: "",
+        }
+      }
     };
   },
   methods: {
     checkData() {
       Object.keys(this.warehouse).forEach((key) => {
-        console.log(key);
         if (this.warehouse[key].value == "") {
           this.warehouse[key].error = this.warehouse[key].label.concat(" is not empty");
         } else {
           this.warehouse[key].error = "";
+        }
+      });
+      Object.keys(this.keyContact).forEach((key) => {
+        if (this.keyContact[key].value == "") {
+          this.keyContact[key].error = this.keyContact[key].label.concat(" is not empty");
+        } else {
+          this.keyContact[key].error = "";
         }
       });
     },
@@ -106,4 +208,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.radius-shadow_add {
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.15);
+  border-radius: 3px;
+}
+</style>
