d9db0328cc717446eab6d2629c9f59ea66cc8647
vtnghia1 <vtnghia110@gmail.com>
2023-06-13 22:07:27 +0700

    fix data base input

--- src/components/Inputs/BaseInput.vue
+++ src/components/Inputs/BaseInput.vue
@@ -1,13 +1,13 @@
 
 <template>
-    <div class="mb-4">
+    <div class="mb-2">
         <label v-if="field.label" :for="field.id" class="!font-bold block">
             <span v-if="field.isRequired == true" class='text-danger'>*</span>{{ field.label }}
         </label>
         <input :id="field.id" :placeholder="field.placeholder" :type="field.type" :value="field.value" @input="updateInput"
             class="p-2 pl-3 relative border rounded-sm w-full focus:!border-gray-700 focus:!ring-gray-700"
             :class="field.error ? `${field.classes} !border-red-500` : field.classes" />
-        <div class="flex justify-between absolute mt-1 ">
+        <div class="flex justify-between mt-1 ">
             <div class="mb-1 text-red-500">{{ field.error }}</div>
             <div v-if="field.maxlength > 0" class="mb-1 text-black-500">{{ field.value.length }}/{{ field.maxlength
             }}
