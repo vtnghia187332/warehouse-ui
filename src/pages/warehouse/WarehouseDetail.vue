978eb8152710a6ac36ce29088194b228e51318d1
tung <vtnghia110@gmail.com>
2023-06-15 15:55:06 +0700

    check data key contact

--- src/pages/warehouse/WarehouseDetail.vue
+++ src/pages/warehouse/WarehouseDetail.vue
@@ -27,24 +27,26 @@
           <div class="px-3 text-primary-85 font-bold text-base">Key Contact</div>
           <div class="mt-3 px-[15px] border-t border-b border-divider content-rate-card">
             <div class="flex justify-between py-3">
-              <span class="text-sm font-bold flex items-center">Key Contact Person (3/3)
+              <span class="text-sm font-bold flex items-center">
               </span>
               <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 @click="HandleAddKeyContact">
                 Add
               </button>
             </div>
-            <div class="bg-neutral-2 w-full border border-neutral-5 rounded-sm mb-4">
+            <div class="bg-neutral-2 w-full border border-neutral-5 rounded-sm mb-4" v-for="(item, index) in keyContact"
+              :key="index">
               <div class="flex justify-between border-b p-3 bg-gray-100">
-                <span class="text-sm font-bold flex items-center ">Key Contact Person
+                <span class="text-sm font-bold flex items-center ">Key Contact Person {{ index + 1 }}
                 </span>
                 <div class="cursor-pointer">
-                  <button class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  <button class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    @click="handleDeleteKeyContact(index)">
                     Delete
                   </button>
                 </div>
               </div>
-              <div class="p-3 bg-gray-100" v-for="(item, index) in keyContact" :key="index">
+              <div class="p-3 bg-gray-100">
                 <div class="flex justify-between">
                   <BaseInput :field="item.title" v-model="item.title.value" />
                 </div>
@@ -249,63 +251,66 @@ export default {
         },
 
       },
-      keyContact: {
-        title: {
-          id: "keycontactTitle",
-          classes: "!w-[540px]",
-          type: "text",
-          label: "Title",
-          isRequired: 'true',
-          value: "",
-          placeholder: "Enter Keycontact Title",
-          maxlength: 0,
-          error: "",
+      keyContact: [
+        {
+          title: {
+            id: "keycontactTitle",
+            classes: "!w-[540px]",
+            type: "text",
+            label: "Title",
+            isRequired: 'true',
+            value: "",
+            placeholder: "Enter Keycontact Title",
+            maxlength: 0,
+            error: "",
+          },
+          firstName: {
+            id: "keycontactFirstName",
+            classes: "!w-[540px]",
+            type: "text",
+            label: "Frist Name",
+            isRequired: 'true',
+            value: "",
+            placeholder: "Enter Keycontact FristName",
+            maxlength: 30,
+            error: "",
+          },
+          lastName: {
+            id: "keycontactLastName",
+            classes: "!w-[540px]",
+            type: "text",
+            label: "Last Name",
+            isRequired: 'true',
+            value: "",
+            placeholder: "Enter Keycontact LastName",
+            maxlength: 30,
+            error: "",
+          },
+          email: {
+            id: "keycontactEmail",
+            classes: "!w-[540px]",
+            type: "text",
+            label: "Email",
+            isRequired: 'true',
+            value: "",
+            placeholder: "Enter Keycontact Email",
+            maxlength: 150,
+            error: "",
+          },
+          phoneNumber: {
+            id: "keycontactPhoneNo",
+            classes: "!w-[540px]",
+            type: "text",
+            label: "Phone Number",
+            isRequired: 'true',
+            value: "",
+            placeholder: "Enter Keycontact Phone Number",
+            maxlength: 150,
+            error: "",
+          }
         },
-        firstName: {
-          id: "keycontactFirstName",
-          classes: "!w-[540px]",
-          type: "text",
-          label: "Frist Name",
-          isRequired: 'true',
-          value: "",
-          placeholder: "Enter Keycontact FristName",
-          maxlength: 30,
-          error: "",
-        },
-        lastName: {
-          id: "keycontactLastName",
-          classes: "!w-[540px]",
-          type: "text",
-          label: "Last Name",
-          isRequired: 'true',
-          value: "",
-          placeholder: "Enter Keycontact LastName",
-          maxlength: 30,
-          error: "",
-        },
-        email: {
-          id: "keycontactEmail",
-          classes: "!w-[540px]",
-          type: "text",
-          label: "Email",
-          isRequired: 'true',
-          value: "",
-          placeholder: "Enter Keycontact Email",
-          maxlength: 150,
-          error: "",
-        },
-        phoneNumber: {
-          id: "keycontactPhoneNo",
-          classes: "!w-[540px]",
-          type: "text",
-          label: "Phone Number",
-          isRequired: 'true',
-          value: "",
-          placeholder: "Enter Keycontact Phone Number",
-          maxlength: 150,
-          error: "",
-        }
-      },
+      ],
+
       address: {
         addressDes: {
           id: "addressDes",
@@ -416,14 +421,20 @@ export default {
       if (index > -1) { // only splice array when item is found
         this.specialDayOn.splice(index, 1); // 2nd parameter means remove one item only
       }
-      console.log(this.specialDayOn);
     },
     handleData(param) {
       this.specialDayOn.push(param);
     },
     HandleAddKeyContact() {
-      this.keyContact.push(this.keyContact);
-    }
+      if (this.keyContact.length < 3) {
+        this.keyContact.push(this.keyContact[0]);
+      }
+    },
+    handleDeleteKeyContact(index) {
+      if (index > 0) { // only splice array when item is found
+        this.keyContact.splice(index, 1); // 2nd parameter means remove one item only
+      }
+    },
   },
 };
 </script>
