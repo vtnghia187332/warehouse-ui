b76cba858aabc8a9223a87f1721e699d9c0253ca
vtnghia1 <vtnghia110@gmail.com>
2023-07-11 23:32:30 +0700

    initiate dialog import

--- src/components/OverrideDialog/ImportDialog.vue
+++ src/components/OverrideDialog/ImportDialog.vue
@@ -1,13 +1,19 @@
 <template>
-    <el-dialog :append-to-body="true" title="Import Warehouse" :before-close="handleCloseDialog"
-        :visible="isOpenDialogImport" width="30%">
-        <el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/" multiple>
-            <i class="el-icon-upload"></i>
-            <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
-            <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
-        </el-upload>
-        <el-button @click="handleCloseDialog">Cancel</el-button>
-    </el-dialog>
+    <div class="import-dlg">
+        <el-dialog :append-to-body="true" title="Import Warehouse" :before-close="handleCloseDialog"
+            :visible="isOpenDialogImport" width="30%" max-width="30%" min-width="30%">
+            <el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/" multiple>
+                <i class="el-icon-upload"></i>
+                <div class="el-upload__text"><b>Click or drag file to this area</b></div>
+                <div class="el-upload__text">Only .xls or .xlsx file format is supported.</div>
+                <div class="el-upload__text">The maximum allow file size is 5MB</div>
+            </el-upload>
+            <div class="flex justify-end">
+                <el-button @click="handleCloseDialog">Cancel</el-button>
+                <el-button class="bg-blue-400 text-white">Import</el-button>
+            </div>
+        </el-dialog>
+    </div>
 </template>
 <script>
 export default {
@@ -24,4 +30,17 @@ export default {
     },
 }
 </script>
-<style scoped></style>
+<style>
+.el-dialog .el-dialog__header {
+    border-bottom: 1px solid rgba(128, 128, 128, 0.322) !important;
+}
+
+.upload-demo .el-upload.el-upload--text {
+    margin: auto !important;
+}
+
+.import-dlg .el-dialog {
+    max-width: 576px !important;
+    min-width: 576px !important;
+}
+</style>
