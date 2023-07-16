536de1330973bd987a76e172b28abe82fa900766
vtnghia1 <vtnghia110@gmail.com>
2023-07-16 10:15:20 +0700

    html css import

--- src/components/OverrideDialog/ImportDialogError.vue
+++ src/components/OverrideDialog/ImportDialogError.vue
@@ -3,15 +3,17 @@
         <el-dialog :append-to-body="true" title="Import Warehouse" :before-close="handleCloseDialog"
             :visible="isOpenDialogImport">
             <div class="flex justify-center">
-                <i class="el-icon-success text-7xl text-green-600"></i>
+                <i v-show="false" class="el-icon-success text-7xl text-green-600"></i>
+                <i v-show="true" class="el-icon-error text-7xl text-red-600"></i>
             </div>
             <div class="flex justify-center mt-4 mb-4">
-                <h2 class="text-2xl">Import successfully</h2>
+                <h2 v-show="false" class="text-2xl">Import successfully</h2>
+                <h2 v-show="true" class="text-2xl">Import unsuccessfully</h2>
             </div>
             <div class="flex justify-center mb-4">
                 <h2>file have been uploaded and information has been update</h2>
             </div>
-            <div class="h-16 bg-gray-100 flex flex-col flex-grow place-content-center pl-3 rounded-sm">
+            <div v-show="false" class="h-16 bg-gray-100 flex flex-col flex-grow place-content-center pl-3 rounded-sm">
                 <div class="mx-auto w-full">
                     <div class="flex items-center">
                         <i class="el-icon-edit-outline text-lg "></i>
@@ -20,14 +22,15 @@
                     </div>
                 </div>
             </div>
-            <div class="h-16 bg-gray-100 flex flex-col flex-grow place-content-center pl-3 rounded-sm mt-2">
+            <div v-show="true" class="h-16 bg-gray-100 flex flex-col flex-grow place-content-center pl-3 rounded-sm mt-2">
                 <div class="mx-auto w-full">
                     <div class="flex items-center">
                         <i class="el-icon-edit-outline text-lg "></i>
                         <span class="ml-1 mr-1 text-red-400">1</span>
                         <span>item with an issue</span>
                     </div>
-                    <div class="text-red-300 text-xs">Item with an issue will not be uploaded and save to servier. Please solve these issues, and reupload file</div>
+                    <div class="text-red-300 font-medium text-xs">Item with an issue will not be uploaded and save to servier. Please
+                        solve these issues, and reupload file</div>
                 </div>
             </div>
             <span slot="footer" class="dialog-footer">
@@ -52,6 +55,9 @@ export default {
     props: {
         isOpenDialogImport: {
             type: Boolean,
+        },
+        field: {
+            type: Object,
         }
     },
     methods: {
